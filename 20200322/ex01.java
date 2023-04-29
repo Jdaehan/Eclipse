5bc6a01226386b8d8d9d9f8feb16a73620e5e4d5
Jdaehan <132067416+Jdaehan@users.noreply.github.com>
2023-04-29 17:10:41 +0900

    Add files via upload

---
+++ 20200322/ex01.java
@@ -0,0 +1,14 @@
+package pro0322;
+
+public class ex01 {
+
+	public static void main(String[] args) {
+		// 주석입니다. 내용을 이곳에 쓰시면 됩니다.
+		System.out.println("이것이 바로 자바 출력구문입니다.");
+		System.out.println("자바가 참 재미있겠죠!!!");
+        int n; //변수 선언, 자료형 선언
+        n=10;
+        System.out.println("n의 값은 m " + n);
+	}
+
+}
