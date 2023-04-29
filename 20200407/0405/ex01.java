781aca1b7f57928c0a8bce500eac02634a6fd743
Jdaehan <132067416+Jdaehan@users.noreply.github.com>
2023-04-29 17:20:17 +0900

    Add files via upload

---
+++ 20200407/0405/ex01.java
@@ -0,0 +1,31 @@
+/*
+package pro0405;
+
+public class ex01 {
+
+	public static void main(String[] args) {
+		// 반복문 예제
+		for(int i=0;i<20;i++)
+		{
+	    System.out.println("java는 흥미로워~~~");	
+		} 
+		//while문으로
+		int i=0;
+		while(i<10) //조건식-최종값
+		{
+			System.out.println("java는 흥미로워~~~");
+			i++; //증감값
+		}
+		
+		int i=0;
+		do
+		{
+		System.out.println("java는 흥미로워~~~"+i);
+		i++; //증감값		
+		}while(i<3);
+
+	}
+
+}
+
+*/
